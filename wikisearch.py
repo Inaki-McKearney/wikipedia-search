637195e090facce84282dedade5cd49db7a12f61
Iñaki McKearney <imckearney01@qub.ac.uk>
2018-01-01 22:23:58 +0000

    Cleaner code

--- wikisearch.py
+++ wikisearch.py
@@ -9,7 +9,8 @@ def query(title):
         response = requests.get("https://en.wikipedia.org/w/api.php", params=parameters)
 
         # Separates definition from the rest of the response
-        intro = response.content.decode("utf-8").split('</span> <span class="s2">')[1].split(
+        intro = \
+        response.content.decode("utf-8").split('t&quot;</span><span class="o">:</span> <span class="s2">')[1].split(
             '</span>')[0]
 
         # For readability
@@ -29,7 +30,7 @@ def query(title):
 def show_image(title):
     try:
         parameters = {'action': 'query', 'prop': 'pageimages', 'format': 'json', 'piprop': 'original', 'titles': title}
-        response = requests.get("http://en.wikipedia.org/w/api.php")
+        response = requests.get("http://en.wikipedia.org/w/api.php", params=parameters)
 
         # Separates url from the rest of the response
         pic = response.content.decode("utf-8").split('"source":"')[1].split('"')[0]
@@ -40,12 +41,13 @@ def show_image(title):
 
 
 if __name__ == '__main__':
-    while (True):
-        search = input("What would you like wikipedia to define?\n").replace(" ", "%20")
+    search = ''
+    while True:
+        search = input("What would you like Wikipedia to define?\n").replace(" ", "%20")
         result = query(search)
-        if ('may refer to:' in result):
+        if 'may refer to:' in result:
             print("Multiple articles: Please be more specific\n")
-        elif (result == 'IndexError'):
+        elif result == 'IndexError':
             print("Page not found. Please try again\n")
         else:
             print(result)
